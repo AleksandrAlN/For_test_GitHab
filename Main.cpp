e1977ded3f425b27d47147b523e2af79f64280f1
AleksandrAlN <102784487+AleksandrAlN@users.noreply.github.com>
2022-04-05 10:45:25 +0300

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,28 @@
+﻿// GitHab2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "Swap.h"
+
+using namespace std;
+
+int main()
+{
+    int x = 10;
+    int y = 15;
+    
+   swap1 (x, y);
+   cout << "swap1 x=" << x << " y=" << y << ";" << endl;
+
+   swap2 ( &x,  &y);
+   cout << "swap2 x=" << x << " y=" << y << ";" << endl;
+
+   swap3(x, y);
+   cout << "Изменить константное значение нельзя." << endl;
+
+   swap4( &x, &y);
+   cout << "Изменить константное значение нельзя." << endl;
+   
+
+   return 0;
+} 
---
+++ Swap.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void swap1 (int& x, int& y);
+void swap2 (int* x, int* y);
+void swap3 (const int& x, const int& y);
+void swap4 (const int* x, const int* y);
