31f86c08e943a1f80feb6b5dcef3d24845d4884b
AleksandrAlN <102784487+AleksandrAlN@users.noreply.github.com>
2022-04-15 14:36:44 +0300

    Delete Main.cpp

--- Main.cpp
+++
@@ -1,9 +0,0 @@
-﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-#include "Engine.h"
-#include <iostream>
-int main()
-{
-	run();
-}
-
