c7d720afcd88116ee5334924af933325ba73edb6
AleksandrAlN <102784487+AleksandrAlN@users.noreply.github.com>
2022-04-15 14:36:54 +0300

    Delete Сalculation.h

--- Сalculation.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <utility>
-using namespace std;
-
-string calculationsPair(pair<string, string>getConvertingTask);
-
